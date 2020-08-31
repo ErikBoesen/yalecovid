cdc4043b738786a8dc4e6d6a0304e5bcad07e3db
ErikBoesen <me@erikboesen.com>
2020-08-31 17:36:31 -0600

    Configure Celery

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import Config
+from celery import Celery
 
 app = Flask(__name__)
 app.config.from_object(Config)
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 
 from app import routes, errors, api
 app.register_blueprint(api.api_blueprint, url_prefix='/api')
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class Config(object):
-    pass
+    CELERY_BROKER_URL = 'redis://localhost:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
