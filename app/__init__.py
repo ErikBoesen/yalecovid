cafd6182dc2cfe1dddf0c3f69e6267875377091b
ErikBoesen <me@erikboesen.com>
2020-08-31 23:47:09 -0600

    Use CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from celery import Celery
 from flask_redis import FlaskRedis
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app.config.from_object(Config)
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 redis = FlaskRedis(app, decode_responses=True)
+cors = CORS(app)
 
 from app import routes, errors, api
 app.register_blueprint(api.api_bp, url_prefix='/api')
